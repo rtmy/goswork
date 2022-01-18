f54dc0268d47f8add7697ab077ecb1abf28a21ae
Artemy <artemy@big3.ru>
2022-01-18 17:27:23 +0300

    added client

---
+++ client.py
@@ -0,0 +1,74 @@
+import os
+from sys import stdin
+
+import requests
+
+
+SERVER_ADDR = '127.0.0.1:8000'
+
+# подключаемся к серверу через requests
+# смотрим список сообщений, выводим его на экран
+# предоставляем пользователю ввод в бесконечном цикле
+# после нажатия Enter, отправляем запрос об отправке на сервер
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+class ConsoleInterface:
+    def check_server_status(self):
+        state_req = requests.get(f'http://{SERVER_ADDR}/chat/state/')
+        if state_req.status_code == 200 and state_req.json().get('state', None) in ('master', 'node'):
+            return state_req.json().get('state', None)
+        return 
+    
+    def check_and_print_server_status(self):
+        state = self.check_server_status()
+        print('Server status:', state if bool(state) else 'FAIL')
+    
+    def retrieve_messages_from_server(self):
+        list_ = requests.get(f'http://{SERVER_ADDR}/chat/list/')
+        self.list_ = list_.json().get('messages', [])
+    
+    def print_messages_from_buffer(self):
+        self.retrieve_messages_from_server()
+        for m in self.list_:
+            print(m)
+    
+    def clear(self):
+        self.list_ = []
+        requests.get(f'http://{SERVER_ADDR}/chat/clear/')
+
+    def send(self, message):
+        requests.get(f'http://{SERVER_ADDR}/chat/send/?message={message}')
+    
+    def init(self):
+        requests.get(f'http://{SERVER_ADDR}/chat/init/')
+        self.check_and_print_server_status()
+    
+    def __call__(self):
+        cls()
+        self.check_and_print_server_status()
+        self.print_messages_from_buffer()
+        print('waiting for input\nprint help to list commands\ninput below:')
+        for line in stdin:
+            cls()
+            self.print_messages_from_buffer()
+            print(line.split('\n'))
+            if line.split('\n')[0] == 'help':
+                print('help - print this message')
+                print('init 127.0.0.1:9000 - try to init connection with 127.0.0.1:9000')
+                print('clear - clear history')
+                print('message - send "message"')
+            elif line.split('\n')[0] == 'clear':
+                self.clear()
+            elif line.split('\n')[0] == 'init':
+                self.init(line.split('\n')[1])
+            else:
+                self.send(line.split('\n')[0])
+            print('input below: ')
+            # print(line, end='')
+
+
+if __name__ == '__main__':
+    c = ConsoleInterface()
+    c()
